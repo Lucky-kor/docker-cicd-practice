2a4f8152af93cc2282b974d66ae4f2da8fda872c
Lucky-kor <jungmin2056@gmail.com>
2025-08-22 18:22:32 +0900

    first commit

---
+++ src/test/java/com/codeit/dockercicdpractice/DockerCicdPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.dockercicdpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCicdPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
